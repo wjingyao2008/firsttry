3d382f1362532e1e9a0391ad606eae1cc64d08b1
wjingyao2008 <317705784@qq.com>
2016-05-04 18:17:52 +0800

    add basic type filter

--- lightreporter/src/main/scala/com/lightreporter/Filter/BasicTypeFilter.scala
+++ lightreporter/src/main/scala/com/lightreporter/Filter/BasicTypeFilter.scala
@@ -17,11 +17,11 @@ class BasicTypeFilter[T](val value:Comparable[T],val repl:String) extends Filter
 
   def getCompare(repl:String)={
     val isPass:(Comparable[T],T)=>Boolean= repl match {
-      case "==" => (t1,t2)=>t1.compareTo(t2)*(-1)==0
-      case ">=" => (t1,t2)=>t1.compareTo(t2)*(-1)>=0
-      case "<=" => (t1,t2)=>t1.compareTo(t2)*(-1)<=0
-      case "<" => (t1,t2)=>t1.compareTo(t2)*(-1)<0
-      case ">" => (t1,t2)=>t1.compareTo(t2)*(-1)>0
+      case "==" => (t1,t2)=>t1.compareTo(t2)==0
+      case ">=" => (t1,t2)=>t1.compareTo(t2)<=0
+      case "<=" => (t1,t2)=>t1.compareTo(t2)>=0
+      case "<" => (t1,t2)=>t1.compareTo(t2)>0
+      case ">" => (t1,t2)=>t1.compareTo(t2)<0
     }
     isPass
   }
